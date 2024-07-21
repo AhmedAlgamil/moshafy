36e9e53da188fcaea42f8a1c97b0d6d0a7e98d6f
Ahmed Gamil Ali <32656250+AhmedAlgamil@users.noreply.github.com>
2024-07-21 20:02:12 +0300

    adding files

---
+++ lib/modules/home_page_screen/data/remote_data_source/local_data_source.dart
@@ -0,0 +1,13 @@
+import 'package:sqflite/sqflite.dart';
+
+import '../../../../core/constatnts/constants.dart';
+import '../../../../core/local/shared_prefrence.dart';
+
+class LocalDataSource {
+  late Database dataBaseQuran;
+
+  LocalDataSource();
+
+
+
+}
---
+++ lib/modules/home_page_screen/data/repository/local_repository.dart
@@ -0,0 +1,7 @@
+import '../remote_data_source/local_data_source.dart';
+
+class LocalRepository {
+  LocalDataSource localDataSource = LocalDataSource();
+
+
+}
